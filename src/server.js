067de53dbc49852faa6362a04865d46ed7471236
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-09-02 16:39:11 +0300

    Installed express and environment set

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const config = require('./config.js');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+
+app.listen(config.PORT, () => console.log(`Server is listening on port ${config.PORT}...`));
