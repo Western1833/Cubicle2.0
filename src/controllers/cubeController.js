a67a1f70bc4f78d7add063b4ab13cf96cda3313d
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-09-09 00:21:12 +0300

    cubes data is now added in db.json

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -2,13 +2,13 @@ const Cube = require('../Models/Cube.js');
 
 const getCreateCube = (req, res) => {
     res.render('create');
-}
+};
 
 const postCreateCube = (req, res) => {
     let newCube = new Cube(req.body);
-
+    Cube.save(newCube);
     res.redirect('/');
-}
+};
 
 module.exports = {
     getCreateCube,
