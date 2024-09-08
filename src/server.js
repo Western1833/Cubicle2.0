342895bcb78007e382e4c6a55018440081fea72c
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-09-08 22:02:43 +0300

    Simplify server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const setViewEngine = require('./config/viewEngine.js');
 
-const config = require('./config.js');
+const config = require('./config/environment.js');
 
 const app = express();
 
+setViewEngine(app);
+
 app.get('/', (req, res) => {
-    res.send('Home page');
+    res.render('index');
 });
 
 app.listen(config.PORT, () => console.log(`Server is listening on port ${config.PORT}...`));
