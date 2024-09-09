c817ee45e8931aea6cd33e5fcfcd848b60dc1cfd
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-09-09 10:03:37 +0300

    Home page is dynamic

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,12 @@
+const db = require('../db.json');
+
 const getHomePage = (req, res) => {
-    res.render('index');
+    res.render('index', {cubes: db.cubes});
 };
 
 const getAboutPage = (req, res) => {
     res.render('about');
-}
+};
 
 module.exports= {
     getHomePage,
