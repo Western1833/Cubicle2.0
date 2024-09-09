b009bb32ea1c9c74d62031f0a1b86389e631828f
Western1833 <145915895+Western1833@users.noreply.github.com>
2024-09-09 15:46:30 +0300

    first part of search is done, searching by name

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,14 @@
 const db = require('../db.json');
 
 const getHomePage = (req, res) => {
-    res.render('index', {cubes: db.cubes});
+    const {search, from, to} = req.query;
+    let cubes = db.cubes;
+
+    if(search){
+        cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+    
+    res.render('index', {cubes, searchedWord: search});
 };
 
 const getAboutPage = (req, res) => {
